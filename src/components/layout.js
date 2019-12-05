ecd824c6edd599c7ba7035b554b94eb3fcfdef64
kkalarasl <kkalarasl@gmail.com>
2019-12-05 23:18:04 +0200

    Adding Navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,17 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
+// import { StaticQuery, graphql } from "gatsby";
+
 import "./bootstrap.min.css"
 import "./layout.css"
 
-const Layout = ({ children }) => <>{children}</>
+import Navbar from "./Globals/Navbar"
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+    {children}
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
